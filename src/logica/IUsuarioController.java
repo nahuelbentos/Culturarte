5dab9a8ac4a03bc581912f35162f0b27b1a2ae6e
Santiago Peraza <santiago.peraza.martinez@fing.edu.uy>
2018-08-25 18:04:33 -0300

    Subo avance en el caso de uso Seguir Usuario. También se agregan excepciones para Regitrar Usuario.

--- src/logica/IUsuarioController.java
+++ src/logica/IUsuarioController.java
@@ -1,15 +1,20 @@
 package logica;
 
 import datatype.DtColaboracion;
-import datatype.DtUsuario;
+import datatype.DtColaborador;
 import datatype.DtProponente;
+import datatype.DtUsuario;
+import excepciones.UsuarioNoExisteElUsuarioException;
+import excepciones.UsuarioYaExisteElUsuarioException;
+import excepciones.UsuarioYaSigueAlUsuarioException;
 
 public interface IUsuarioController {
 
-	public abstract boolean agregarUsuario(DtUsuario dtNuevoUsuario);
+	public abstract void agregarUsuario(DtUsuario dtNuevoUsuario) throws UsuarioYaExisteElUsuarioException;
 	
-	// Crear el DtColaborador
-	//public abstract DtColaborador[] listarColaboradores();
+	public abstract DtUsuario verPerfilUsuario(String nickname) throws UsuarioNoExisteElUsuarioException;
+	
+	public abstract DtColaborador[] listarColaboradores();
 	
 	// Revisar si están bien los parámetros que recibe
 	public abstract DtColaboracion listarColaboracion(String titulo, String nickname);
@@ -17,13 +22,17 @@ public interface IUsuarioController {
 	// Revisar si es necesario el DtPerfilColaborador
 	//public abstract DtPerfilColaborador verPerfilColaborador(String nickname);
 	
+	public abstract DtUsuario[] listarUsuarios();
+	
+	public abstract DtUsuario[] listarUsuariosQueSigue(String nickname);
+	
 	public abstract DtProponente[] listarProponentes();
 	
 	// Revisar si es necesario el DtPerfilProponente
 	//public abstract DtPerfilProponente verPerfilProponente(String nickname);
 	
 	// Revisar si están bien los parámetros que recibe
-	public abstract boolean seguirUsuario(String nicknameUno, String nicknameDos);
+	public abstract void seguirUsuario(String nicknameUno, String nicknameDos) throws UsuarioYaSigueAlUsuarioException;
 	
 	// Revisar si están bien los parámetros que recibe
 	public abstract boolean dejarDeSeguirUsuario(String nicknameUno, String nicknameDos);
