2d98691d580a5dd4881db41e2df1ea07458c66f2
Santiago Peraza <santiago.peraza.martinez@fing.edu.uy>
2018-08-27 23:59:42 -0300

    Se agrega prueba de persistencia para la creación de Usuarios.

--- src/logica/Colaborador.java
+++ src/logica/Colaborador.java
@@ -1,13 +1,21 @@
 package logica;
 
-import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("C")
 public class Colaborador extends Usuario {
 
-	public Colaborador(String nickname, String nombre, GregorianCalendar fechaNacimiento, String correoElectronico,
+	public Colaborador() {
+		super();
+	}
+	
+	public Colaborador(int id, String nickname, String nombre, GregorianCalendar fechaNacimiento, String correoElectronico,
 			String apellido, String imagen) {
-		super(nickname, nombre, fechaNacimiento, correoElectronico, apellido, imagen);
+		super(id, nickname, nombre, fechaNacimiento, correoElectronico, apellido, imagen);
 	}
 
 
